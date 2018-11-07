aaf276c5b908971a2add7d7da2b89f44a994ebb7
Luc de Brouwer <lljdebrouwer@gmail.com>
2018-11-07 01:37:15 +0100

    tweaked validation a bit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,29 @@ $(document).ready(function(){
     $('#name').focus();
 
 /* start Basic info validation */
-    $('#name').on('input', function() {
+
+// This function will be called on form submit, which is located at the end of this file
+function checkBasicOnSubmit() {
+
+    const $name = $('#name');
+    if($name.val() == '') {
+        $name.removeClass('valid').addClass('invalid');
+        $('fieldset').children()[1].innerHTML = "Name: " + "<span class='invalid'>* please provide your name</span>";
+    } else {
+        $name.removeClass('invalid').addClass('valid');
+    }
+
+    const $email = $('#mail');
+    if($email.val() == '') {
+        $email.removeClass('valid').addClass('invalid');
+        $('fieldset').children()[3].innerHTML = "Mail: " + "<span class='invalid'>* please provide a valid email address</span>";
+    } else {
+        $email.removeClass('invalid').addClass('valid');
+    }
+}
+
+// Validate on live user input
+$('#name').on('input', function() {
         let errorMessage = "Name: " + "<span id='errorName'>* please provide your name</span>";
         let input = $(this);
         let name = input.val();
@@ -19,6 +41,7 @@ $(document).ready(function(){
 
         } else {
 
+        // If field is empty, append an error message into the name label
         $('fieldset').children()[1].innerHTML = errorMessage;
         $('#errorName').addClass('invalid');
             input.removeClass('valid').addClass("invalid");
@@ -26,6 +49,7 @@ $(document).ready(function(){
         }
     });
 
+// Validate on live user input
     $('#mail').on('input', function() {
         let eMessageNotValid = "Email: " + "<span id='errorMail'>* Please provide a valid email address</span>";
         let eMessageEmpty =  'Email: ' + "<span id='errorEmpty'>Please fill in your email</span>";
@@ -34,18 +58,24 @@ $(document).ready(function(){
         if(mail && mail.indexOf == 1) {
             input.removeClass('invalid').addClass('valid');
         }
-        
+        // Check if the user provided an @ in the email input
         if(mail.indexOf('@') != -1) {
             $('fieldset').children()[3].innerHTML = 'Email: ';
             $('#errorMail').removeClass('invalid');
             input.removeClass('invalid').addClass('valid');
         } 
          else {
+
+            // Check if user left email field blank
              if(mail == '') {
+
+                // Clear the label
+                // Append the empty input error message
                 $('fieldset').children()[3].innerHTML = '';
                 $('fieldset').children()[3].innerHTML = eMessageEmpty;
                 $('#errorEmpty').addClass('invalid');
              } else {
+                // If there is no @ in the input, append the incorrect email error message
                 $('fieldset').children()[3].innerHTML = eMessageNotValid;
                 $('#errorMail').addClass('invalid');
                 input.removeClass('valid').addClass("invalid");
@@ -108,13 +138,11 @@ $('#design').on('change', function(event){
 const activities = $('.activities label');
 const checkboxes = $('.activities input[type="checkbox"]');
 
+// Compare selected activity with other selected activity
+
+ // Foreach activity selected, check its content
 
 
-// for(let i = 0; activities.length; i+=1) {
-
-//     
-//     console.log(span);
-// }
 
 
 
@@ -131,10 +159,117 @@ As a user selects activities, a running total should display below the list of c
 
 $('#payment option[value="select_method"').attr('disabled', true);
 $('#payment option[selected]').removeAttr('selected');
-$('#payment option[value="credit card"]').attr('selected', 'selected');
+$('#payment option[value="credit card"]').attr('selected', true);
 
 $('fieldset div:contains("PayPal")').hide();
 $('fieldset div:contains("Bitcoin")').hide();
+
+// This function is called on form submit, can be found at the end of this file
+function checkCreditCardDetailsOnSubmit() {
+    // Select the creditcard details 
+    const ccInput = $('#cc-num');
+    const ccZip = $('#zip');
+    const creditCvv = $('#cvv');
+
+
+
+    // If one ore more fields is empty, mark them as invalid
+    // In all other cases, the fields are not empty, check on input.
+
+        if(ccInput.val() == '') {
+            ccInput.removeClass('valid').addClass('invalid');
+        } else {
+            let num = new RegExp('^[0-9]{13,16}$');
+            if(num.test(ccInput.val())) {
+                ccInput.removeClass('invalid').addClass('valid');
+            } else {
+                ccInput.removeClass('valid').addClass('invalid');
+            }
+        }
+    
+        if(ccZip.val() == '') {
+            ccZip.removeClass('valid').addClass('invalid');
+        } else {
+            let zip = new RegExp('^[0-9]{5}$');
+            if(zip.test(ccZip.val())) {
+                ccZip.removeClass('invalid').addClass('valid');
+            } else {
+                ccZip.removeClass('valid').addClass('invalid');
+            }
+        }
+    
+        if(creditCvv.val() == '') {
+            creditCvv.removeClass('valid').addClass('invalid');
+        } else {
+            let ccv = new RegExp('^[0-9]{3}$');
+            if(ccv.test(creditCvv.val())) {
+                creditCvv.removeClass('invalid').addClass('valid');           
+            } else {
+                creditCvv.removeClass('valid').addClass('invalid');
+            }
+        }
+        // If all fields are empty, mark them as invalid
+        if(ccInput.val() == '' && ccZip.val() == ''  && creditCvv.val() == '') {
+            ccInput.removeClass('valid').addClass('invalid');
+            ccZip.removeClass('valid').addClass('invalid');
+            creditCvv.removeClass('valid').addClass('invalid');
+        }
+
+  
+}
+function checkCreditCardDetails() {
+    // Select each respective creditcard details
+    // Check on (live) user input
+    $('#cc-num').on('input', function() {
+        let input = $(this);
+        let ccNum = input.val();
+        
+        // Check whether user input meets Regex
+        // Creditcard num should only accept 13-16 digits
+        // If valid, mark valid
+        // Else mark invalid
+        let num = new RegExp('^[0-9]{13,16}$');
+        if(num.test(ccNum)) {
+            input.removeClass('invalid').addClass('valid');
+        } else {
+            input.removeClass('valid').addClass('invalid');
+        }
+
+        console.log(ccNum);
+    });
+    $('#zip').on('input', function() {
+        let input = $(this);
+        let ccZip = input.val();
+
+        // Check whether user input meets Regex
+        // ZIP should only accept 5 digits
+        let zip = new RegExp('^[0-9]{5}$');
+        if(zip.test(ccZip)) {
+            input.removeClass('invalid').addClass('valid');
+        } else {
+            input.removeClass('valid').addClass('invalid');
+        }
+    });
+    
+    $('#cvv').on('input', function() {
+        let input = $(this);
+        let creditCvv = input.val();
+        // Check whether user input meets Regex
+        // CCV should only accept 3 digits
+        let ccv = new RegExp('^[0-9]{3}$');
+        if(ccv.test(creditCvv)) {
+            input.removeClass('invalid').addClass('valid');           
+        } else {
+            input.removeClass('valid').addClass('invalid');
+        }
+    });
+}
+
+// Check validation on default selection
+if($('#payment option[value="credit card"]').attr('selected', true)) {
+    checkCreditCardDetails();
+}
+
 
 $('#payment').on('change', (event) => {
     if(event.target.value == 'credit card') {
@@ -143,6 +278,8 @@ $('#payment').on('change', (event) => {
         $('fieldset div:contains("PayPal")').hide();
         $('fieldset div:contains("Bitcoin")').hide();
 
+        // Validate on selection
+        checkCreditCardDetails();
     } else if (event.target.value == 'paypal') {
         // Hide creditcard and bitcoin
         $('#credit-card').hide();
@@ -164,8 +301,7 @@ $('#payment').on('change', (event) => {
 
     $('form').on('submit', (e) => {
         e.preventDefault();
-
-        // Check if fields are empty and are valid
-        //checkValidation();
+        checkBasicOnSubmit();
+        checkCreditCardDetailsOnSubmit();
     })
  });
